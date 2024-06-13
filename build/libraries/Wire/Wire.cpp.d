65248be3994db3e6233d37c6638b6f1aad1b8f61
Окунев Иван <ianwarets@mail.ru>
2024-06-13 20:41:30 +0300

    Removed build folder.

--- build/libraries/Wire/Wire.cpp.d
+++
@@ -1,8 +0,0 @@
-C:\Users\ianwa\OneDrive\Документы\Arduino\Sketches\TelemetryForRacing\build\libraries\Wire\Wire.cpp.o: \
- C:\Users\ianwa\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\Wire.cpp \
- C:\Users\ianwa\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility/twi.h \
- C:\Users\ianwa\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\Wire.h \
- C:\Users\ianwa\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Stream.h \
- C:\Users\ianwa\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Print.h \
- C:\Users\ianwa\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/WString.h \
- C:\Users\ianwa\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Printable.h
--- build/libraries/Wire/utility/twi.c.d
+++
@@ -1,6 +0,0 @@
-C:\Users\ianwa\OneDrive\Документы\Arduino\Sketches\TelemetryForRacing\build\libraries\Wire\utility\twi.c.o: \
- C:\Users\ianwa\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility\twi.c \
- C:\Users\ianwa\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
- C:\Users\ianwa\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/binary.h \
- C:\Users\ianwa\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\variants\standard/pins_arduino.h \
- C:\Users\ianwa\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility\twi.h
